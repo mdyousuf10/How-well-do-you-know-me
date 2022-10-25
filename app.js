33607449b29f6e7403b56ea520eae7654aff77a9
mdyousuf10 <70437376+mdyousuf10@users.noreply.github.com>
2022-10-25 20:57:06 +0530

    Build quiz app

---
+++ app.js
@@ -0,0 +1,74 @@
+const readlineSync = require("readline-sync")
+var score = 0;
+
+// data of high score
+var highScores = [
+  {
+    name: "Anwar",
+    score: 3,
+  },
+
+  {
+    name: "shafee",
+    score: 2,
+  },
+]
+
+// array of objects
+var questions = [{
+  question: "Where do I live? ",
+  answer: "Aurangabad"
+}, {
+  question: "My favorite superhero would be? ",
+  answer: "Ironman"
+},
+{
+  question: "Where will I am going work? ",
+  answer: "capgemini"
+}];
+
+function welcome() {
+  var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome " + userName + " DO YOU KNOW Yousuf?");
+}
+
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("right!");
+    score = score + 1;
+
+  } else {
+    console.log("wrong!");
+
+  }
+
+  console.log("current score: ", score);
+  console.log("-------------")
+}
+
+function game() {
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+
+welcome();
+game();
+showScores();
+
+
